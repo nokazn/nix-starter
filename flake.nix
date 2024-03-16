3b4a2e71c696162dd4b4b21cd9114ec1fdc70268
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-16 20:50:54 +0900

    feat: add rust template

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "nix-starter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = with pkgs; pkgs.mkShell {
+        buildInputs = [
+          nixpkgs-fmt
+        ];
+      };
+    });
+}
